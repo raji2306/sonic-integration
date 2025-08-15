a6a97de93f435777d93f2afc4114cf4e40f265c4
raji2306 <94977452+raji2306@users.noreply.github.com>
2025-08-15 22:22:55 +0530

    Update App.test.js

--- react-frontend/src/App.test.js
+++ react-frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders navbar with Home link', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const homeLink = screen.getByText(/home/i); // matches your navbar
-  expect(homeLink).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
