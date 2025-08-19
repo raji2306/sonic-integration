bdb3ffe83ffbba09658ef637c96eea5fd2676216
raji2306 <94977452+raji2306@users.noreply.github.com>
2025-08-19 15:29:47 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "db_username" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_name" {
+  type    = string
+  default = "devops"
+}
