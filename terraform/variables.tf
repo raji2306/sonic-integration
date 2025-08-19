1ff4778a7abf6af2edd90b017341b8a0eaef380a
raji2306 <94977452+raji2306@users.noreply.github.com>
2025-08-19 21:53:05 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,17 @@
 variable "db_username" {
   description = "RDS username"
   type        = string
+  default = "admin"
 }
 
 variable "db_password" {
   description = "RDS password"
   type        = string
   sensitive   = true
+}
+
+variable "aws_region" {
+  description = "AWS region where RDS will be created"
+  type        = string
+  default     = "ap-south-1"
 }
