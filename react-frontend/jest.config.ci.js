2b7c573b3ebc9065fa8a007ae0ab9a13acf7d373
raji2306 <94977452+raji2306@users.noreply.github.com>
2025-08-24 21:02:32 +0530

    Update jest.config.ci.js

--- react-frontend/jest.config.ci.js
+++ react-frontend/jest.config.ci.js
@@ -1,11 +1,16 @@
 module.exports = {
-  testEnvironment: "jsdom",
+  testEnvironment: "jsdom", // For React components
   transformIgnorePatterns: ["node_modules/(?!(axios)/)"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy"
   },
   reporters: [
     "default",
-    ["jest-junit", { outputDirectory: "./test-reports", outputName: "frontend-test-report.xml" }]
-  ]
+    ["jest-junit", {
+      outputDirectory: "./test-reports",
+      outputName: "frontend-test-report.xml"
+    }]
+  ],
+  // Optional: avoid leaving async handles open (like API calls)
+  // setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
 };
