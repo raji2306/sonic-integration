904432bb53d50a3cd026b8fe34706c6eaea68bc5
raji2306 <94977452+raji2306@users.noreply.github.com>
2025-08-25 10:36:42 +0530

    Create providers.tf

---
+++ terraform/sonarqube/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "sonic-integration-bucket"
+    key            = "sonarqube/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "dynamo"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = 'ap-south-1'
+}
